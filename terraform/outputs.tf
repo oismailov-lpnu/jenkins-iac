94c7ce5ad79fe474821fcf7d5a09971c876754f4
oismailov-exco <oleh.d.ismailov@lpnu.ua>
2025-12-08 00:05:24 +0200

    test

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
 output "vm_public_ips_by_name" {
   description = "Map of instance name -> external IP"
   value = {
-    for inst in google_compute_instance.cheap_vm :
+    for inst in google_compute_instance.web :
     inst.name => inst.network_interface[0].access_config[0].nat_ip
   }
 }
@@ -9,13 +9,13 @@ output "vm_public_ips_by_name" {
 # (Optional, if you still want them separately)
 output "vm_names" {
   description = "Names of created instances"
-  value       = google_compute_instance.cheap_vm[*].name
+  value       = google_compute_instance.web[*].name
 }
 
 output "vm_external_ips" {
   description = "External IP addresses"
   value = [
-    for nic in google_compute_instance.cheap_vm[*].network_interface[0].access_config[0] :
+    for nic in google_compute_instance.web[*].network_interface[0].access_config[0] :
     nic.nat_ip
   ]
 }
